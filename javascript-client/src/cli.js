7eff74885053b8c409264dd8738958dd916e8521
robbiemu <roberto.tomas.cuentas@gmail.com>
2016-06-24 16:31:50 -0400

    client 1.0

--- javascript-client/src/cli.js
+++ javascript-client/src/cli.js
@@ -1,14 +1,16 @@
 import net from 'net'
 import vorpal from 'vorpal'
 
+const Palette = require('./palette')
+
 const cli = vorpal()
 
 import vorpalLogger from 'vorpal-log'
-const chalk = require('chalk')
 
 const LONG_NAME = 'ftd-chat'
 const DEFAULT_DELIMITER = 'ftd-chat~$'
 var ADDRESS = {}
+var Vars = {}
 
 // cli config
 cli.use(vorpalLogger)
@@ -23,6 +25,12 @@ cli
   .mode('connect <port> [host]')
   .delimiter('connected:')
   .init(function (args, callback) {
+    cli._baseExitMode = cli._exitMode
+    cli._exitMode = function (args) {
+      cli.delimiter(DEFAULT_DELIMITER)
+      return cli._baseExitMode(args)
+    }
+
     server = net.createConnection(args, () => {
       ADDRESS = server.address()
       Log.log(`connected to server ${ADDRESS.address}:${ADDRESS.port}`)
@@ -30,13 +38,29 @@ cli
     })
 
     server.on('data', (data) => {
-      if (data.toString().match(/^\*{4}/)) {
-        let out = data.toString().replace(/\*{4}/,"")
+      if (data.toString().match(/^\*(?:.*)\*(?:.*)\*(?:.*)$/)) {
+        let arr = /^\*(.*)\*(.*)\*(.*)$/.exec(data.toString())
+        let colorname = arr[1]
+        let messagetype = arr[2]
+        let message = arr[3]
 
-        Log.info(chalk.underline.bgBlue(out))
-        if (data.toString().match(/Username set to:/)) {
-          var arr = /Username set to: (.*)$/.exec(data.toString())
-          cli.delimiter(arr[1] + '@' + ADDRESS.address)
+        let output = Palette.getColor(colorname)(message)
+
+        if (messagetype.length > 0) {
+          Log.log(output)
+        } else {
+          Log.info(output)
+        }
+
+        switch (messagetype) {
+          case 'username':
+            let arr = /Username set to: (.*)$/.exec(data.toString())
+            Vars.username = arr[1]
+            cli.delimiter(arr[1] + '@' + ADDRESS.address)
+            break
+          case 'disconnect':
+            cli.execSync('exit')
+            break
         }
       } else {
         Log.log(data.toString())
@@ -46,6 +70,11 @@ cli
     server.on('end', () => {
       Log.log('server disconnected')
     })
+
+    server.on('error', args => {
+      let err = args.err
+      Log.error(err)
+    })
   })
   .action(function (command, callback) {
     server.write(command + '\n')
@@ -54,13 +83,7 @@ cli
 
 const exit = cli.find('exit')
 if (exit) {
-  Log.log('bingo')
   exit.description('Exits ' + LONG_NAME)
-  exit._after = function () {
-    // TODO - make this actually happen. _after is not triggering
-    server.end()
-    cli.delimiter(DEFAULT_DELIMITER)
-  }
 }
 
 export default cli
